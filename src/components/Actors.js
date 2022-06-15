7be2692b4709d9c0e44fde4b6c56f85d9a22fa69
Kyrian <dr.kyrian.onwe@gmail.com>
2022-06-14 20:04:34 -0500

    commit tree 8c18b3842586134d340abe45f3cd219a752bdcc4

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,15 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}<h1>Actors Page</h1>
+  {actors.map((act) => <div key={act.name}>
+      {act.name}
+      <ul>
+        {act.movies.map((mov) => <li key={mov}>{mov}</li>)}
+      </ul>
+    </div>
+  )}
+  </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,15 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}<h1>Directors Page</h1>
+    {directors.map((dir) => <div key={dir.name}>
+        {dir.name}
+        <ul>
+          {dir.movies.map((mov) => <li key={mov}>{mov}</li>)}
+        </ul>
+      </div>
+    )}
+  </div>;
 }
 
 export default Directors;
